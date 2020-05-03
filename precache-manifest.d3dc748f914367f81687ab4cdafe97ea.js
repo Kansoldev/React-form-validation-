6373792df81f540a998ddfe942f42f61da86f795
Oyinkansola <yahayaoyinkansola@gmail.com>
2020-05-03 07:27:46 +0100

    Updates

---
+++ precache-manifest.d3dc748f914367f81687ab4cdafe97ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f23a3f0e8d0a9ee1ed33a4d20da39f39",
+    "url": "/React-form-validation/index.html"
+  },
+  {
+    "revision": "daadd9efadf6d9b43bd9",
+    "url": "/React-form-validation/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "2f636aa9d17b356a7751",
+    "url": "/React-form-validation/static/css/main.35f3145b.chunk.css"
+  },
+  {
+    "revision": "daadd9efadf6d9b43bd9",
+    "url": "/React-form-validation/static/js/2.76ee4d15.chunk.js"
+  },
+  {
+    "revision": "2f636aa9d17b356a7751",
+    "url": "/React-form-validation/static/js/main.4a30b1fa.chunk.js"
+  },
+  {
+    "revision": "8b83f57f1d557a43e1e5",
+    "url": "/React-form-validation/static/js/runtime-main.53154638.js"
+  }
+]);
